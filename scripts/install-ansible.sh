4c5df351716958059b12aeb13c9b4d9be4ab6645
Jsarrazin <jonathan.sarrazin@imt-atlantique.net>
2022-03-29 15:54:14 +0200

    Update

--- scripts/install-ansible.sh
+++ scripts/install-ansible.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 sudo apt-get update -y
 sudo apt install python3-pip -y
-sudo apt install software-properties-common
+sudo apt install software-properties-common -y
 sudo add-apt-repository --yes --update ppa:ansible/ansible
-sudo apt install ansible
+sudo apt install ansible -y
 #SSH
 sudo apt-get -y install sshpass
