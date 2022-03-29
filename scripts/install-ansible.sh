f42a66152b2b704f57d6cc34d3ca8dd470441aac
Jsarrazin <jonathan.sarrazin@imt-atlantique.net>
2022-03-29 15:51:25 +0200

    Update

--- scripts/install-ansible.sh
+++ scripts/install-ansible.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
-sudo rm /var/lib/apt/lists/lock
-sudo rm /var/cache/apt/archives/lock
-sudo rm /var/lib/dpkg/updates/*
 sudo apt-get update -y
 sudo apt install python3-pip -y
-sudo apt-get install ansible -y
-sudo pip3 install ansible
-sudo pip3 install pywinrm
-sudo apt-get install python3-winrm -y
+sudo apt install software-properties-common
+sudo add-apt-repository --yes --update ppa:ansible/ansible
+sudo apt install ansible
 #SSH
 sudo apt-get -y install sshpass
---
+++ scripts/install-traitor.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+git clone https://github.com/liamg/traitor.git  /opt/traitor
+cd  /opt/traitor
+cp traitor /usr/bin/traitor
+
+
