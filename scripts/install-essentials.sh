ba7c6211ec58868edc3909036080304ba6da0a3b
Jsarrazin <jonathan.sarrazin@imt-atlantique.net>
2022-05-17 10:53:04 +0200

    Update

--- scripts/install-essentials.sh
+++ scripts/install-essentials.sh
@@ -4,6 +4,7 @@ apt install feroxbuster -y
 apt install seclists -y
 tar -xzf /usr/share/seclists/Passwords/Leaked-Databases/rockyou.txt.tar.gz --directory /usr/share/seclists/Passwords/Leaked-Databases && rm rockyou.txt.tar.gz 
 apt install golang-go -y
+apt install kali-tools-*
 
 # Creating Nice folders
 mkdir -p /home/kali/Documents/PrivEsc/{scans,tools}
