27780d3bc55e441200b59ad1d19e11bab5be8ef9
Jsarrazin <jonathan.sarrazin@imt-atlantique.net>
2022-10-10 13:07:55 +0200

    Update

--- install.sh
+++ install.sh
@@ -27,15 +27,6 @@ apt install python3-pip -y
 pip3 install virtualenv
 apt install sshpass -y
 
-echo 'Install Python2..'
-apt install -y build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev xz-utils tk-dev libffi-dev liblzma-dev python3-openssl git
-curl https://pyenv.run | bash
-echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.zshrc
-echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.zshrc
-echo -e 'if command -v pyenv 1>/dev/null 2>&1; then\n  eval "$(pyenv init --path)"\nfi' >> ~/.zshrc
-exec $SHELL
-pyenv install 2.7.18
-
 echo "All set. Hack the planet!"
 
 cd ..
