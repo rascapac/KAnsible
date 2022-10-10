637e5d7c06484675ebdd0ac60df1d13d6d2e4912
Jsarrazin <jonathan.sarrazin@imt-atlantique.net>
2022-10-10 15:15:16 +0200

    Update

--- scripts/install-tool.sh
+++ scripts/install-tool.sh
@@ -11,12 +11,12 @@ apt install bloodhound
 /bin/neo4j start&
 
 #Python 2
-echo 'Install Python2..'
-apt install -y build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev xz-utils tk-dev libffi-dev liblzma-dev python3-openssl git
-curl https://pyenv.run | bash
-echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.zshrc
-echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.zshrc
-echo -e 'if command -v pyenv 1>/dev/null 2>&1; then\n  eval "$(pyenv init --path)"\nfi' >> ~/.zshrc
-exec $SHELL
-pyenv install 2.7.18
+#echo 'Install Python2..'
+#apt install -y build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev xz-utils tk-dev libffi-dev liblzma-dev python3-openssl git
+#curl https://pyenv.run | bash
+#echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.zshrc
+#echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.zshrc
+#echo -e 'if command -v pyenv 1>/dev/null 2>&1; then\n  eval "$(pyenv init --path)"\nfi' >> ~/.zshrc
+#exec $SHELL
+#pyenv install 2.7.18
 
